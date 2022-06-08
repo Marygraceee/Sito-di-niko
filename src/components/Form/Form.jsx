7f270b4d48d2c98697e3cd867f1b0bfa464b4611
Marygrace <ma.grasso1999@gmail.com>
2022-06-08 16:27:08 +0200

    Form adapts to smartphones and tablets

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -23,44 +23,65 @@ function Form() {
   };
 
   return (
-    <div className="formContainer">
+    <div className="Container">
 
-      <form className="zonaForm" ref={form} onSubmit={sendEmail}>
-        <div>
-          <label htmlFor="name">Nome ciao!</label>
-          <input type="text" id="name" name="user_name" />
-        </div>
+      <div className="zonaForm">
+        <form ref={form} onSubmit={sendEmail}>
+          <div>
+            <label htmlFor="name">Nome</label>
+            <input type="text" id="name" name="user_name" />
+          </div>
 
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="user_email" />
-        </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="user_email" />
+          </div>
 
-        <div>
-          <label htmlFor="message">La tua richiesta</label>
-          <textarea type="text" name="message" id="message" cols="60" rows="5" style={{ resize: 'none' }} />
-        </div>
+          <div>
+            <label htmlFor="message">La tua richiesta</label>
+            <textarea type="text" name="message" id="message" cols="60" rows="5" style={{ resize: 'none' }} />
+          </div>
 
-        <div>
-          <button className="sendBtn" type="submit">Invia</button>
-        </div>
+          <div>
+            <button className="sendBtn" type="submit">Invia</button>
+          </div>
 
-      </form>
+          <div className="socialBonus">
+            <div>
+              <h1>Email</h1>
+              <a href="mailto:nikolececcarelli@gmail.com">nikolececcarelli@gmail.com</a>
+            </div>
+            <div>
+              <h1>Telefono</h1>
+              <p>Italia</p>
+              <p>+39 3884748205</p>
+            </div>
+            <div>
+              <h1>Social</h1>
+              <a href="https://www.linkedin.com/in/nikolececcarelli/" target="_blank" rel="noreferrer">LinkedIn</a>
+            </div>
+          </div>
+
+        </form>
+      </div>
 
       <div className="socialContainer">
-        <div>
-          <h1>Email</h1>
-          <a href="mailto:nikolececcarelli@gmail.com">nikolececcarelli@gmail.com</a>
+        <div className="socialQuadrato">
+          <div>
+            <h1>Email</h1>
+            <a href="mailto:nikolececcarelli@gmail.com">nikolececcarelli@gmail.com</a>
+          </div>
+          <div>
+            <h1>Telefono</h1>
+            <p>Italia</p>
+            <p>+39 3884748205</p>
+          </div>
+          <div>
+            <h1>Social</h1>
+            <a href="https://www.linkedin.com/in/nikolececcarelli/" target="_blank" rel="noreferrer">LinkedIn</a>
+          </div>
         </div>
-        <div>
-          <h1>Telefono</h1>
-          <p>Italia</p>
-          <p>+39 3884748205</p>
-        </div>
-        <div>
-          <h1>Social</h1>
-          <a href="https://www.linkedin.com/in/nikolececcarelli/" target="_blank" rel="noreferrer">LinkedIn</a>
-        </div>
+
       </div>
     </div>
   );
