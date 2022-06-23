a9fa656127d693e857c8f1b13ecfddd582fac761
Marygraceee <ma.grasso1999@gmail.com>
2022-06-23 17:55:48 +0200

    Starting services section

--- src/components/Servizi/Servizi.jsx
+++ src/components/Servizi/Servizi.jsx
@@ -1,8 +1,96 @@
 import React from 'react';
+import placeholder from '../../assets/placeholder.jpg';
+import './Servizi.css';
 
 function Servizi() {
   return (
-    <div>Servizi</div>
+    <div className="servicesContainer">
+
+      <div className="service">
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Inventore temporibus rem alias provident beatae corrupti a,
+            ipsam quae sunt! Earum obcaecati expedita provident voluptatibus
+            eaque, in nesciunt quidem praesentium aut voluptatum ex facilis
+            maxime debitis optio asperiores inventore nobis officia voluptate
+            ducimus? Deserunt, adipisci sunt aspernatur ducimus ea quisquam
+            voluptatibus?
+
+          </p>
+          <img src={placeholder} alt="" />
+        </div>
+
+      </div>
+
+      <div className="service">
+        <div className="card">
+          <img src={placeholder} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Inventore temporibus rem
+            alias provident beatae corrupti a, ipsam quae
+            sunt! Earum obcaecati expedita provident voluptatibus
+            eaque, in nesciunt quidem praesentium aut voluptatum
+            ex facilis maxime debitis optio asperiores inventore
+            nobis officia voluptate ducimus? Deserunt, adipisci
+            sunt aspernatur ducimus ea quisquam voluptatibus?
+
+          </p>
+        </div>
+
+      </div>
+
+      <div className="service">
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Inventore temporibus rem alias provident beatae corrupti a,
+            ipsam quae sunt! Earum obcaecati expedita provident voluptatibus
+            eaque, in nesciunt quidem praesentium aut voluptatum ex facilis
+            maxime debitis optio asperiores inventore nobis officia voluptate
+            ducimus? Deserunt, adipisci sunt aspernatur ducimus ea quisquam
+            voluptatibus?
+
+          </p>
+          <img src={placeholder} alt="" />
+        </div>
+      </div>
+
+      <div className="service">
+        <div className="card">
+          <img src={placeholder} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Inventore temporibus rem
+            alias provident beatae corrupti a, ipsam quae
+            sunt! Earum obcaecati expedita provident voluptatibus
+            eaque, in nesciunt quidem praesentium aut voluptatum
+            ex facilis maxime debitis optio asperiores inventore
+            nobis officia voluptate ducimus? Deserunt, adipisci
+            sunt aspernatur ducimus ea quisquam voluptatibus?
+
+          </p>
+        </div>
+      </div>
+
+      <div className="service">
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Inventore temporibus rem alias provident beatae corrupti a,
+            ipsam quae sunt! Earum obcaecati expedita provident voluptatibus
+            eaque, in nesciunt quidem praesentium aut voluptatum ex facilis
+            maxime debitis optio asperiores inventore nobis officia voluptate
+            ducimus? Deserunt, adipisci sunt aspernatur ducimus ea quisquam
+            voluptatibus?
+
+          </p>
+          <img src={placeholder} alt="" />
+        </div>
+      </div>
+
+    </div>
   );
 }
 
